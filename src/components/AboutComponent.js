c5b969608519300dd9a7ecdcb376fa066bb3a233
maxjacobzander <maxjacobzander@gmail.com>
2021-05-11 15:26:15 -0400

    Created About component and page. Added About component to Home page and route to App.js. Renamed photo of Sarah at booth

---
+++ src/components/AboutComponent.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import SarahImg from '../assets/images/main_images/Sarah at Adrians Dream Desk.jpeg'
+
+const AboutComponent = () => {
+
+    const AboutDiv = styled.div`
+        background: #a3d1e7;
+        height: auto;
+        width: 100%;
+    `
+
+    const Welcome = styled.h1`
+        font-family: turbinado-pro, sans-serif;
+        font-weight: 700;
+        font-style: normal;
+        text-align: center;
+        ${'' /* margin-top: 10%; */}
+        color: #1a3959;
+        font-size: 6rem;
+    `
+
+    const Blurb = styled.p`
+        font-family: factoria, serif;
+        font-weight: 300;
+        font-style: normal;
+        text-align: center;
+        align-items: center;
+        font-size: 1.7rem;
+        line-height: 2;
+        color: #1a3959;
+        width: 78%;
+        margin: auto;
+    `
+
+    const SarahDiv = styled.div`
+        position: relative;
+    `
+
+    const Signature = styled.h1`
+        font-family: turbinado-pro, sans-serif;
+        font-size: 7rem;
+        text-align: center;
+        color: #1a3959;
+    `
+
+    const SarahPic = styled.img`
+        height: 400px;
+        width: auto;
+        border-radius: 50%
+    `
+
+    return (
+        <AboutDiv>
+            <Welcome>
+                <br />
+                welcome to adrian's dream
+            </Welcome>
+            <Blurb>
+            This business is dedicated to my son Adrian.
+            He passed before we could get to know him and before he could have any brothers and/or sisters.
+            His life revealed to us a genetic condition that affects my health and the health of any future children.
+            I’ve created Adrian’s Dream to support my IVF journey and my sons dream of having siblings.
+            <br /><br/>
+            Thank you for reading and thank you for your support!
+            </Blurb>
+            <SarahDiv>
+                <Signature>
+                    Sarah Vega
+                </Signature>
+                <SarahPic src={SarahImg} alt="Sarah at booth" />
+            </SarahDiv>
+        </AboutDiv>
+    );
+}
+
+export default AboutComponent;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from '../Main';
+import About from './About';
 
 const Home = () => {
     return (
         <>
         <Main />
+        <About />
         </>
     );
 }
