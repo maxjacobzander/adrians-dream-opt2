897b6f88a72ea9cd4211bfd6e574c8744dd11b9a
maxjacobzander <maxjacobzander@gmail.com>
2021-05-19 19:22:40 -0400

    Removed routes to other components to keep to single page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-import About from './components/About';
-import Gallery from './components/Gallery';
-import Events from './components/Events';
 
 function App() {
   require('dotenv').config()
@@ -14,10 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/about' exact component={About}/>
-          <Route path='/gallery' exact component={Gallery} />
-          <Route path='/events' exact component={Events} />
+          <Route path='/' component={Home}/>
         </Switch>
       </Router>
     </div>
