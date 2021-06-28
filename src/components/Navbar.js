e279c67c42e01a29c1b42c499fa610540dd67207
maxjacobzander <maxjacobzander@gmail.com>
2021-06-27 21:34:45 -0400

    Removed commented out {Link} in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import './Navbar.css';
-// import { Link } from 'react-router-dom';
 import { Link } from 'react-scroll';
 import LogoText from '../assets/images/logos/AdriansDreamLogoText.jpeg';
 
