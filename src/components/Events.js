d6a399c0571a67a8208af37a64b64e01ecdd3637
maxjacobzander <maxjacobzander@gmail.com>
2021-08-01 22:23:25 -0400

    Moved Events styling to separate component

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,74 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import * as Styled from './Events.style';
 import CardItem from './CardItem';
 import './Cards.css';
 import ThursdaysOnMain from '../assets/images/art_generic/Thursdays on Main.png';
 import PolkMakersMarket from '../assets/images/art_generic/Polk County Makers Market.jpeg';
 
 const Events = () => {
-    const UpcomingEventsDiv = styled.div`
-        background: #eee3b1;
-        font-size: 20px;
-        margin-top: -35px;
-        margin-bottom: 0;
-        width: 100%;
-        height: auto;
-        padding-bottom: 50px;
-    `
-
-    const EventsHeader = styled.h1`
-        font-family: turbinado-pro, sans-serif;
-        font-weight: 700;
-        font-style: normal;
-        text-align: center;
-        color: #1a3959;
-        font-size: 6rem;
-        margin-top: -18px;
-        padding-top: 50px;
-        @media screen and (max-width: 1025px) {
-            line-height: .9;
-        }
-    `
-
-    const EventsBlurb = styled.h3`
-         width: 80%;
-         align-items: center;
-         text-align: justify;
-         margin: auto;
-         font-family: factoria, serif;
-         font-weight: 300;
-         font-style: normal;
-         font-size: 1.7rem;
-         line-height: 2;
-         color: #1a3959;
-         @media screen and (max-width: 1025px) {
-            font-size: 20px;
-            text-align: center;
-        }
-    `
-
-    const CardsContainer = styled.div`
-        align-items: center;
-        width: 90%;
-        margin: 0 auto;
-    `
-
-    const CardWrapper = styled.div`
-        position: relative;
-        margin: 50px 0 45px;
-    `
-
-
     return (
-        <UpcomingEventsDiv id="events">
-            <EventsHeader>upcoming events</EventsHeader>
-            <EventsBlurb>
+        <Styled.UpcomingEventsDiv id="events">
+            <Styled.EventsHeader>upcoming events</Styled.EventsHeader>
+            <Styled.EventsBlurb>
             In addition to our workshops, Adrian's Dream is a frequent vendor at craft fairs throughout the state of Iowa! Find us at a craft fair near you to check our current crafts; to commission a special piece just for you; or to just say hi!
             <br/>
             <p style={{textAlign:"center"}}>Check back here for both upcoming craft fair appearances and workshop dates/locations!</p>
-            </EventsBlurb>
-            <CardsContainer>
-                <CardWrapper>
+            </Styled.EventsBlurb>
+            <Styled.CardsContainer>
+                <Styled.CardWrapper>
                     <ul className="cards__items">
                         <CardItem
                         src={ThursdaysOnMain}
@@ -95,9 +42,9 @@ const Events = () => {
                         link="https://www.jesterparknaturecenter.com/calendar-events/fall-makers-market/?fbclid=IwAR0qhNiegEa49U9LSuc_asPPYaa30NKzeSHGLt0drRB88fSzVgXFVQg01zs"
                         />
                     </ul>
-                </CardWrapper>
-            </CardsContainer>
-        </UpcomingEventsDiv>
+                </Styled.CardWrapper>
+            </Styled.CardsContainer>
+        </Styled.UpcomingEventsDiv>
     );
 }
 
---
+++ src/components/Events.style.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const UpcomingEventsDiv = styled.div`
+background: #eee3b1;
+font-size: 20px;
+margin-top: -35px;
+margin-bottom: 0;
+width: 100%;
+height: auto;
+padding-bottom: 50px;
+`
+
+export const EventsHeader = styled.h1`
+font-family: turbinado-pro, sans-serif;
+font-weight: 700;
+font-style: normal;
+text-align: center;
+color: #1a3959;
+font-size: 6rem;
+margin-top: -18px;
+padding-top: 50px;
+@media screen and (max-width: 1025px) {
+    line-height: .9;
+}
+`
+
+export const EventsBlurb = styled.h3`
+ width: 80%;
+ align-items: center;
+ text-align: justify;
+ margin: auto;
+ font-family: factoria, serif;
+ font-weight: 300;
+ font-style: normal;
+ font-size: 1.7rem;
+ line-height: 2;
+ color: #1a3959;
+ @media screen and (max-width: 1025px) {
+    font-size: 20px;
+    text-align: center;
+}
+`
+
+export const CardsContainer = styled.div`
+align-items: center;
+width: 90%;
+margin: 0 auto;
+`
+
+export const CardWrapper = styled.div`
+position: relative;
+margin: 50px 0 45px;
+`
